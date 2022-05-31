f4afd1a26a1c5c2067e865a5bd89a07c2860df11
tsegamlakp <99799402+tsegamlakp@users.noreply.github.com>
2022-05-31 23:42:46 +0300

    done

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,50 +1,44 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <unistd.h>
+
 /**
- * insert_node - inserts a number into a sorted singly linked list
- * @head: pointer to the initial linked list pointer
- * @number: number to insert
- * Return: pointer to the new node
+ * insert_node - inserts a number in an ordered linked list
+ * @head: double pointer to the linked list
+ * @number: number to insert in the new node
+ *
+ * Return: address of the new node, or NULL
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new, *tmp;
-	int i, j;
+	listint_t *current = *head;
+	listint_t *new = NULL;
+	listint_t *temp = NULL;
+
+	if (!head)
+		return (NULL);
 
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
-	new->n = number, tmp = *head;
-	if (!(*head))
-		return (*head = new, new->next = NULL, new);
-	for (i = 0; tmp; i++)
+	new->n = number;
+	new->next = NULL;
+
+	if (!*head || (*head)->n > number)
 	{
-		if (number > tmp->n)
-		{
-			if (!tmp->next)
-				return (tmp->next = new, new->next = NULL, new);
-			tmp = tmp->next;
-			continue;
-		}
-		else
-		{
-			new->next = tmp;
-			if (tmp == *head)
-			{
-				*head = new;
-				break;
-			}
-			tmp = *head;
-			for (j = 0; j < i; j++)
-			{
-				if (j == (i - 1))
-				{
-					tmp->next = new;
-					break;
-				}
-				tmp = tmp->next;
-			}
-			break;
-		}
+		new->next = *head;
+		return (*head = new);
 	}
+	else
+	{
+		while (current && current->n < number)
+		{
+			temp = current;
+			current = current->next;
+		}
+		temp->next = new;
+		new->next = current;
+	}
+
 	return (new);
 }
