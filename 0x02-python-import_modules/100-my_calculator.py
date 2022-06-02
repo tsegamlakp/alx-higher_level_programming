2f105d515a5d2736f38fab89a6204fdd07670851
tsegamlakp <99799402+tsegamlakp@users.noreply.github.com>
2022-06-02 21:27:10 +0300

    done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
-
-
-def main():
-
-    n = len(sys.argv)
-    ops = [add, sub, mul, div]
-    if (n != 4):
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    operator = sys.argv[2]
-    if operator not in ["+", "-", "*", "/"]:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-    for i, op in enumerate(["+", "-", "*", "/"]):
-        if op is operator:
-            print("{} {} {} = {}".format(a, operator, b, ops[i](a, b)))
-
-
 if __name__ == "__main__":
-    main()
+    from sys import argv
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        quit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    ops = ["+", "-", "*", "/"]
+    from calculator_1 import add, sub, mul, div
+    funcs = [add, sub, mul, div]
+    for i, s in enumerate(ops):
+        if argv[2] == s:
+            print("{} {} {} = {}".format(a, s, b, funcs[i](a, b)))
+            break
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        quit(1)
