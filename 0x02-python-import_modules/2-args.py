f53afdacdd6e1c0a0b12dd057fde57c3a6f71824
tsegamlakp <99799402+tsegamlakp@users.noreply.github.com>
2022-06-02 23:04:08 +0300

    done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-
-def main():
-
-    n = len(sys.argv)
-    if (n == 1):
-        print("0 arguments.")
-    elif n == 2:
-        print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
-    else:
-        print("{} arguments:".format(n - 1))
-        for i, arg in enumerate(sys.argv[1:]):
-            print("{}: {}".format(i + 1, arg))
-
-
 if __name__ == "__main__":
-    main()
+    import sys
+    """ number of arguments without the file name """
+    arguments = len(sys.argv) - 1
+    if arguments == 0:
+        print("0 arguments.")
+    elif arguments == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(arguments))
+    for count in range(arguments):
+        print("{}: {}".format(count + 1, sys.argv[count + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-import sys
-
-
-def add():
-
-    s = 0
-    for i in sys.argv[1:]:
-        s += int(i)
-    print(s)
-
-
 if __name__ == "__main__":
-    add()
+    import sys
+    add = 0
+    number = len(sys.argv)
+    for solution in range(1, number):
+        add += int(sys.argv[solution])
+    print(add)
