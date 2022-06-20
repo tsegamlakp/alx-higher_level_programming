06b2b709eecf5c8db4add7c6b6a37b7f3e550f9c
tsegamlakp <99799402+tsegamlakp@users.noreply.github.com>
2022-06-20 18:31:03 +0300

    done

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-safe_print_integer_err = \
-    __import__('100-safe_print_integer_err').safe_print_integer_err
-
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
+© 2022 GitHub, Inc.
+Terms
+Privacy
