3802338746592c5e09309a588080eeb164e1ee65
tsegamlakp <99799402+tsegamlakp@users.noreply.github.com>
2022-06-20 18:21:35 +0300

    done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,11 @@
-
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    cnt = 0
+    try:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end='')
+            cnt += 1
+    except IndexError:
+        pass
+    print()
+    return (cnt)
