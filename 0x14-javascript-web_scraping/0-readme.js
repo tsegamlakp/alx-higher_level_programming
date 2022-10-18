9680d571511ae9026d847f4cc76005dd8a3aba56
tsegamlakp <99799402+tsegamlakp@users.noreply.github.com>
2022-10-18 21:38:29 +0300

    done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+
+fs.readFile(process.argv[2], 'utf-8', function (error, data) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
 });
